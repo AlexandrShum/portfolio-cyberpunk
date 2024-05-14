3934639a83317073f2fb3cc46391d0b509bcd57a
Alexandr Shumakov <tcuxrdr@gmail.com>
2024-05-14 19:05:43 +0300

    basePath ?

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
-  // basePath: isProd ? "/portfolio-cyberpunk" : "",
+  basePath: isProd ? "/portfolio-cyberpunk" : "",
   images: {
     unoptimized: true
   },
