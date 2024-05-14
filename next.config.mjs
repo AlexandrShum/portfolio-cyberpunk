919db4c5e2b87fb46711ad5337339806f755a4cf
Alexandr Shumakov <tcuxrdr@gmail.com>
2024-05-14 19:03:02 +0300

    try

--- next.config.mjs
+++ next.config.mjs
@@ -5,11 +5,11 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
-  basePath: isProd ? "/portfolio-cyberpunk" : "",
+  // basePath: isProd ? "/portfolio-cyberpunk" : "",
   images: {
     unoptimized: true
   },
-  assetPrefix: isProd ? "/portfolio-cyberpunk" : "",
+  // assetPrefix: isProd ? "/portfolio-cyberpunk" : "",
 };
 
 export default nextConfig;
