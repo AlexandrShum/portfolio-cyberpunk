a8a63b2d0acd0c018ca4b2db47627bd023ee55b1
Alex Shumakov <tcuxrdr@gmail.com>
2026-07-16 17:05:15 +0300

    Try to fix pictures

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,7 @@
 export const getImageUrl = (imgUrl: string): string => {
-  const prefix = "/portfolio-cyberpunk";
-  const isProd = process.env.NODE_ENV === "production";
+  // const prefix = "/portfolio-cyberpunk";
+  // const isProd = process.env.NODE_ENV === "production";
 
-  return isProd ? `${prefix}` + imgUrl : imgUrl;
+  // return isProd ? `${prefix}` + imgUrl : imgUrl;
+  return imgUrl;
 }
