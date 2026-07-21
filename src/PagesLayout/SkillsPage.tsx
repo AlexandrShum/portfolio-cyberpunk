8816b4d748ca8af561965c7e482bfa43ccb26cff
Alex Shumakov <tcuxrdr@gmail.com>
2026-07-21 14:30:40 +0300

    Fix skill section layout

--- src/PagesLayout/SkillsPage.tsx
+++ src/PagesLayout/SkillsPage.tsx
@@ -14,24 +14,46 @@ import { getImageUrl } from "../utils";
 
 export const SkillsPage: FC = () => {
   return (
-    <div className="flex md:h-[100vh] h-[375px] md:max-h-[1080px] relative md:overflow-hidden w-full">
+    <div className="flex h-[full] md:h-[100vh] md:max-h-[1080px] relative md:overflow-hidden w-full">
       <BlockWithBorderLeft />
       <div
         className="hidden md:block bg-100 bg-no-repeat w-[23%] inner-shadow"
-        style={{ backgroundImage: `url(${getImageUrl("/skills_page_picture.png")})`}}
+        style={{
+          backgroundImage: `url(${getImageUrl('/skills_page_picture.png')})`,
+        }}
       ></div>
 
       <BlockWithBorderRight>
         <Header />
-        <div className=" m-auto pt-[10%] w-[70%] relative z-20">
+        <div className=" m-auto pt-[10%] w-[70%] h-full relative z-20">
           <SkillSection />
         </div>
       </BlockWithBorderRight>
+
+      {/* Mobile View */}
       <MobileHeader />
       <div className="relative md:hidden w-full h-full pt-[56px] flex flex-col justify-between items-end">
-        <Image src={getImageUrl("/scheme.png")} alt="scheme" width={760} height={700} className="-z-30 absolute top-0"/>
-        <Image src={getImageUrl("/elipse-mobile-right.png")} alt="elipse" width={300} height={700} className="-z-30 absolute top-[50px] right-[0px]" />
-        <Image src={getImageUrl("/elipse-mobile-left.png")} alt="elipse" width={300} height={700} className="-z-30 absolute top-[250px] left-[0px]" />
+        <Image
+          src={getImageUrl('/scheme.png')}
+          alt="scheme"
+          width={760}
+          height={700}
+          className="-z-30 absolute top-0"
+        />
+        <Image
+          src={getImageUrl('/elipse-mobile-right.png')}
+          alt="elipse"
+          width={300}
+          height={700}
+          className="-z-30 absolute top-[50px] right-[0px]"
+        />
+        <Image
+          src={getImageUrl('/elipse-mobile-left.png')}
+          alt="elipse"
+          width={300}
+          height={700}
+          className="-z-30 absolute top-[250px] left-[0px]"
+        />
         <SkillSection />
       </div>
     </div>
--- src/components/SkillsPage/SkillsSection.tsx
+++ src/components/SkillsPage/SkillsSection.tsx
@@ -6,22 +6,29 @@ import { skillsData, SkillDataInterface } from "./Skills/data";
 import { getImageUrl } from "../../utils"; 
 
 export const SkillSection: FC = () => (
-  <div>
+  <div className="h-full pb-10">
+    {/* Mobile */}
     <div
       className="md:hidden w-full h-[80px] bg-100 bg-no-repeat flex items-center justify-end"
-      style={{ backgroundImage: `url(${getImageUrl("/skills-mobile-hero.png")})`}}
+      style={{
+        backgroundImage: `url(${getImageUrl('/skills-mobile-hero.png')})`,
+      }}
     >
       <div className="pr-10 text-xl font-semibold text-titleColor">
-        {"My skills"}
+        {'My skills'}
       </div>
     </div>
-    <div className="flex flex-wrap justify-between px-4 py-8 md:pt-0 gap-5 md:gap-6 md:pb-14">
-      {skillsData.map((skill: SkillDataInterface, i: number) => (
-        <SkillsBlock {...skill} key={i} />
-      ))}
-    </div>
-    <div>
-      <ExperienceHighlights />
+
+    {/* Desktop */}
+    <div className="w-full h-full md:max-h-[80%] scrollable-container pr-4">
+      <div className="flex flex-wrap justify-between px-4 py-8 md:pt-0 gap-5 md:gap-6 md:pb-14">
+        {skillsData.map((skill: SkillDataInterface, i: number) => (
+          <SkillsBlock {...skill} key={i} />
+        ))}
+      </div>
+      <div>
+        <ExperienceHighlights />
+      </div>
     </div>
   </div>
 );
